e0ac471ebba04809f7fde5d56ad0f22a8a9bc9fe
eejit <eejitgames@gmail.com>
2024-05-12 23:18:44 +0100

    configurable wall thickness

--- app/main.rb
+++ app/main.rb
@@ -15,6 +15,9 @@ end
 
 # room numbers in the range of 0 - 1023, starting room is 339, the arrangement of rooms is 4 rooms tall, by 256 rooms wide
 def check_movement
+  if inputs.keyboard.key_down.forward_slash
+    state.show_boxes = !state.show_boxes
+  end
   if args.inputs.keyboard.left
     state.room_number -= 1
     state.room_number = 1023 if state.room_number == -1
@@ -37,6 +40,7 @@ def draw_room
   draw_closed_door
   draw_outer_walls
   draw_inner_walls
+  draw_wall_junctions if state.show_boxes
 end
 
 # player can't go straight back out the way they came in
@@ -47,29 +51,45 @@ end
 def draw_outer_walls
   draw_wall_segment(x: 0, y: 0, dir: :N)
   draw_wall_segment(x: 0, y: 0, dir: :E)
-  draw_wall_segment(x: 254, y: 0, dir: :E)
-  draw_wall_segment(x: 762, y: 0, dir: :E)
-  draw_wall_segment(x: 1016, y: 0, dir: :E)
-  draw_wall_segment(x: 1270, y: 0, dir: :N)
-  draw_wall_segment(x: 0, y: (10 + (680/3)) * 2, dir: :N)
-  draw_wall_segment(x: 1270, y: (10 + (680/3)) * 2, dir: :N)
-  draw_wall_segment(x: 0, y: (10 + (680/3)) * 3, dir: :E)
-  draw_wall_segment(x: 254, y: (10 + (680/3)) * 3, dir: :E)
-  draw_wall_segment(x: 762, y: (10 + (680/3)) * 3, dir: :E)
-  draw_wall_segment(x: 1016, y: (10 + (680/3)) * 3, dir: :E)
+  draw_wall_segment(x: state.segment_width + state.wall_width, y: 0, dir: :E)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: 0, dir: :E)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 4, y: 0, dir: :E)
+  draw_wall_segment(x: 1280 - state.wall_width, y: 0, dir: :N)
+  draw_wall_segment(x: 0, y: (state.segment_height + state.wall_height) * 2, dir: :N)
+  draw_wall_segment(x: 1280 - state.wall_width, y: (state.segment_height + state.wall_height) * 2, dir: :N)
+  draw_wall_segment(x: 0, y: (state.segment_height + state.wall_height) * 3, dir: :E)
+  draw_wall_segment(x: state.segment_width + state.wall_width, y: (state.segment_height + state.wall_height) * 3, dir: :E)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: (state.segment_height + state.wall_height) * 3, dir: :E)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 4, y: (state.segment_height + state.wall_height) * 3, dir: :E)
 end
 
 # draw inner walls in room, forming a simple maze with wide corridors
 def draw_inner_walls
   state.wall_seed = state.room_number
-  draw_wall_segment(x: 254, y: (10 + (680/3)) * 2, dir: get_direction)
-  draw_wall_segment(x: 508, y: (10 + (680/3)) * 2, dir: get_direction)
-  draw_wall_segment(x: 762, y: (10 + (680/3)) * 2, dir: get_direction)
-  draw_wall_segment(x: 1016, y: (10 + (680/3)) * 2, dir: get_direction)
-  draw_wall_segment(x: 254, y: 10 + (680/3), dir: get_direction)
-  draw_wall_segment(x: 508, y: 10 + (680/3), dir: get_direction)
-  draw_wall_segment(x: 762, y: 10 + (680/3), dir: get_direction)
-  draw_wall_segment(x: 1016, y: 10 + (680/3), dir: get_direction)
+  draw_wall_segment(x: state.segment_width + state.wall_width, y: (state.segment_height + state.wall_height) * 2, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 2, y: (state.segment_height + state.wall_height) * 2, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: (state.segment_height + state.wall_height) * 2, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 4, y: (state.segment_height + state.wall_height) * 2, dir: get_direction)
+  draw_wall_segment(x: state.segment_width + state.wall_width, y: state.segment_height + state.wall_height, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 2, y: state.segment_height + state.wall_height, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: state.segment_height + state.wall_height, dir: get_direction)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 4, y: state.segment_height + state.wall_height, dir: get_direction)
+end
+
+def draw_wall_junctions
+  start_x = 0
+  start_y = 0
+  count = 0
+
+  while count < 25
+    outputs.solids  << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    start_x += state.segment_width + state.wall_width
+    if start_x > 1280
+      start_x = 0
+      start_y += state.segment_height + state.wall_height
+    end
+    count += 1
+  end
 end
 
 # this is a version of the generation system used in the arcade game berzerk - it follows the same patterns as the arcade game following a reset.
@@ -100,17 +120,21 @@ end
 def draw_wall_segment(x:, y:, dir:)
   case dir
   when :N
-    outputs.solids  << { x: x, y: y, w: 10, h: 20 + (680/3), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x, y: y, w: state.wall_width, h: state.segment_height + (2 * state.wall_height), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   when :S
-    outputs.solids  << { x: x, y: y - ((10 + (680/3))), w: 10, h: 20 + (680/3), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x, y: y - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   when :E
-    outputs.solids  << { x: x, y: y, w: 264, h: 10, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x, y: y, w: state.segment_width + (2 * state.wall_width), h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   when :W
-    outputs.solids  << { x: x - 254, y: y, w: 264, h: 10, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x - state.segment_width, y: y, w: state.segment_width + state.wall_width, h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   end
 end
 
 def set_defaults
+  state.wall_width = 18
+  state.wall_height = 18
+  state.segment_height = (720 - (4 * state.wall_height)) / 3
+  state.segment_width = (1280 - (6 * state.wall_width)) / 5
   state.custom_tick_count = 0
   state.room_number = 0x0153
   state.defaults_set = true
