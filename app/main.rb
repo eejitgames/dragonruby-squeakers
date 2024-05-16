ad5b9d6f325edd6efab0d8620f40da91b3ded4bb
eejit <eejitgames@gmail.com>
2024-05-16 18:53:08 +0100

    fix wall positioning bug

--- app/main.rb
+++ app/main.rb
@@ -34,9 +34,11 @@ end
 
 # function to draw all the walls for a given room
 def draw_room
+  state.array_of_wall_hashes = []
   draw_closed_door
   draw_outer_walls
   draw_inner_walls
+  # draw_wall_junctions
 end
 
 # player can't go straight back out the way they came in
@@ -50,9 +52,9 @@ def draw_outer_walls
   draw_wall_segment(x: state.segment_width + state.wall_width, y: 0, dir: :E)
   draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: 0, dir: :E)
   draw_wall_segment(x: (state.segment_width + state.wall_width) * 4, y: 0, dir: :E)
-  draw_wall_segment(x: 1280 - state.wall_width, y: 0, dir: :N)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 5, y: 0, dir: :N)
   draw_wall_segment(x: 0, y: (state.segment_height + state.wall_height) * 2, dir: :N)
-  draw_wall_segment(x: 1280 - state.wall_width, y: (state.segment_height + state.wall_height) * 2, dir: :N)
+  draw_wall_segment(x: (state.segment_width + state.wall_width) * 5, y: (state.segment_height + state.wall_height) * 2, dir: :N)
   draw_wall_segment(x: 0, y: (state.segment_height + state.wall_height) * 3, dir: :E)
   draw_wall_segment(x: state.segment_width + state.wall_width, y: (state.segment_height + state.wall_height) * 3, dir: :E)
   draw_wall_segment(x: (state.segment_width + state.wall_width) * 3, y: (state.segment_height + state.wall_height) * 3, dir: :E)
@@ -73,15 +75,15 @@ def draw_inner_walls
 end
 
 def draw_wall_junctions
-  start_x = 0
-  start_y = 0
+  start_x = state.x_offset
+  start_y = state.y_offset
   count = 0
 
   while count < 24
     outputs.solids  << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
     start_x += state.segment_width + state.wall_width
     if start_x > 1280
-      start_x = 0
+      start_x = state.x_offset
       start_y += state.segment_height + state.wall_height
     end
     count += 1
@@ -116,21 +118,26 @@ end
 def draw_wall_segment(x:, y:, dir:)
   case dir
   when :N
-    outputs.solids  << { x: x, y: y, w: state.wall_width, h: state.segment_height + (2 * state.wall_height), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    state.array_of_rect_hashes << { x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height) }
+    # puts "#{state.array_of_rect_hashes}"
+    # puts "============================="
   when :S
-    outputs.solids  << { x: x, y: y - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   when :E
-    outputs.solids  << { x: x, y: y, w: state.segment_width + (2 * state.wall_width), h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset, w: state.segment_width + (2 * state.wall_width), h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   when :W
-    outputs.solids  << { x: x - state.segment_width, y: y, w: state.segment_width + state.wall_width, h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids  << { x: x + state.x_offset - state.segment_width, y: y + state.y_offset, w: state.segment_width + state.wall_width, h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
   end
 end
 
 def set_defaults
   state.wall_width = 16
   state.wall_height = 16
-  state.segment_height = (720 - (4 * state.wall_height)) / 3
-  state.segment_width = (1280 - (6 * state.wall_width)) / 5
+  state.segment_height = (720 - (4 * state.wall_height)).div(3) # drop after decimal, 2 pixels shorter than / 3
+  state.segment_width = (1280 - (6 * state.wall_width)).div(5) # drop after decimal, 4 pixels shorter than / 5
+  state.x_offset = ((1280 - (6 * state.wall_width)) - (state.segment_width * 5)) / 2 # 2 # half the dropped portion
+  state.y_offset = ((720 - (4 * state.wall_height)) - (state.segment_height * 3)) / 2 # 1 # half the dropped portion
   state.custom_tick_count = 0
   state.room_number = 0x0153
   state.defaults_set = true
