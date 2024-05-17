5dab2fafb2c3a7375261829a46017f4b15980d4d
eejit <eejitgames@gmail.com>
2024-05-17 20:29:51 +0100

    rough code, create upside down room array

--- app/main.rb
+++ app/main.rb
@@ -34,12 +34,15 @@ end
 
 # function to draw all the walls for a given room
 def draw_room
-  state.array_of_wall_hashes = []
+  # state.array_of_wall_rects = []
   draw_closed_door
   draw_outer_walls
   draw_inner_walls
   # draw_wall_junctions
   # draw_diagonal_test
+  # puts state.array_of_wall_rects
+  # puts "==========="
+  # create_room_array
 end
 
 # player can't go straight back out the way they came in
@@ -81,7 +84,7 @@ def draw_wall_junctions
   count = 0
 
   while count < 24
-    outputs.solids  << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
     start_x += state.segment_width + state.wall_width
     if start_x > 1280
       start_x = state.x_offset
@@ -97,10 +100,34 @@ def draw_diagonal_test
   count = 0
 
   while start_x < 1280
-    outputs.solids  << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids << { x: start_x, y: start_y, w: state.wall_width, h: state.wall_height, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
     start_x += state.wall_width * 2
     start_y += state.wall_height
   end
+end
+
+def create_room_array
+  start_x = state.x_offset
+  start_y = state.y_offset
+  count = 0
+
+  while start_y < 720 - 16
+    outputs.solids << { x: start_x, y: start_y, w: state.wall_width / 2, h: state.wall_height / 2, r: 100, g: 100, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    
+    if geometry.find_intersect_rect( { x: start_x, y: start_y, w: state.wall_width / 2, h: state.wall_height / 2 }, state.array_of_wall_rects )
+      print 1
+    else
+      print 0
+    end
+    
+    start_x += state.wall_width
+    if start_x > 1280 - (28 + 16)
+      start_x = state.x_offset
+      start_y += state.wall_height
+      print "\n"
+    end
+  end
+  puts "====="
 end
 
 # this is a version of the generation system used in the arcade game berzerk - it follows the same patterns as the arcade game following a reset.
@@ -131,28 +158,26 @@ end
 def draw_wall_segment(x:, y:, dir:)
   case dir
   when :N
-    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
-    state.array_of_rect_hashes << { x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height) }
-    # puts state.array_of_rect_hashes
-    # puts "============================="
+    outputs.solids << { x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    state.array_of_wall_rects += [{ x: x + state.x_offset, y: y + state.y_offset, w: state.wall_width, h: state.segment_height + (2 * state.wall_height)}]
   when :S
-    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids << { x: x + state.x_offset, y: y + state.y_offset - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2), r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    state.array_of_wall_rects += [{ x: x + state.x_offset, y: y + state.y_offset - state.segment_height - state.wall_height, w: state.wall_width, h: state.segment_height + (state.wall_height * 2)}]
   when :E
-    outputs.solids  << { x: x + state.x_offset, y: y + state.y_offset, w: state.segment_width + (2 * state.wall_width), h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids << { x: x + state.x_offset, y: y + state.y_offset, w: state.segment_width + (2 * state.wall_width), h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    state.array_of_wall_rects += [{ x: x + state.x_offset, y: y + state.y_offset, w: state.segment_width + (2 * state.wall_width), h: state.wall_height}]
   when :W
-    outputs.solids  << { x: x + state.x_offset - state.segment_width, y: y + state.y_offset, w: state.segment_width + state.wall_width, h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    outputs.solids << { x: x + state.x_offset - state.segment_width, y: y + state.y_offset, w: state.segment_width + state.wall_width, h: state.wall_height, r: 10, g: 10, b: 200, a: 255, anchor_x: 0, anchor_y: 0, blendmode_enum: 1 }
+    state.array_of_wall_rects += [{ x: x + state.x_offset - state.segment_width, y: y + state.y_offset, w: state.segment_width + state.wall_width, h: state.wall_height}]
   end
 end
 
 def set_defaults
+  state.array_of_wall_rects = []
   state.wall_width = 16
   state.wall_height = 16
-  # state.segment_height = (720 - (4 * state.wall_height)).div(3) # drop after decimal, 2 pixels shorter than / 3
-  # state.segment_width = (1280 - (6 * state.wall_width)).div(5) # drop after decimal, 4 pixels shorter than / 5
   state.segment_height = 16 * 13
   state.segment_width = 16 * 14
-  # state.x_offset = ((1280 - (6 * state.wall_width)) - (state.segment_width * 5)) / 2 # 2 # half the dropped portion
-  # state.y_offset = ((720 - (4 * state.wall_height)) - (state.segment_height * 3)) / 2 # 1 # half the dropped portion
   state.x_offset = 28
   state.y_offset = 16
   state.custom_tick_count = 0
