12d099892478524f9a5d7047a1d983cf8e8866bd
eejit <eejitgames@gmail.com>
2024-05-24 16:16:32 +0100

    update sprite auto-tiling at wall junctions

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -18,11 +18,10 @@ def draw_room
   state.room_grid ||= Array.new(state.rows) { Array.new(state.cols, 0) }
   draw_outer_wall_solids
   draw_inner_wall_solids
-
-  draw_wall_debug
-
-  #draw_outer_wall_sprites
-  #draw_inner_wall_sprites
+  # draw_wall_debug
+  draw_outer_wall_sprites
+  draw_inner_wall_sprites
+  draw_wall_junctions_sprites
 end
 
 # room numbers in the range of 0 - 1023, starting room is 339, the arrangement of rooms is 4 rooms tall, by 256 rooms wide
@@ -100,22 +99,20 @@ def draw_outer_wall_sprites
   outputs.sprites <<  { x: 76 * 16, y: 42 * 16 - 16, w: 48, h: 48, path: "sprites/wall_10.png" }
   outputs.sprites <<  { x: 76 * 16, y: 2 * 16, w: 48, h: 48, path: "sprites/wall_3.png" }
 
-  outputs.sprites << { x: 31 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_6.png" }
-  outputs.sprites << { x: 31 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_6.png" }
-  outputs.sprites << { x: 46 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_6.png" }
-  outputs.sprites << { x: 46 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_6.png" }
+  outputs.sprites << { x: 31 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y: 3)}.png" }
+  outputs.sprites << { x: 31 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y: 42)}.png" }
+  outputs.sprites << { x: 46 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y: 3)}.png" }
+  outputs.sprites << { x: 46 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y: 42)}.png" }
 
   outputs.sprites << { x: 16 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_6.png" }
   outputs.sprites << { x: 16 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_6.png" }
   outputs.sprites << { x: 61 * 16,  y: 2 * 16,  w: 48, h: 48, path: "sprites/wall_6.png" }
   outputs.sprites << { x: 61 * 16,  y: 41 * 16, w: 48, h: 48, path: "sprites/wall_6.png" }
 
-  outputs.sprites << { x: 1 * 16,   y: 15 * 16, w: 48, h: 48, path: "sprites/wall_8.png" }
-  outputs.sprites << { x: 1 * 16,   y: 28 * 16, w: 48, h: 48, path: "sprites/wall_1.png" }
-  outputs.sprites << { x: 76 * 16,  y: 15 * 16, w: 48, h: 48, path: "sprites/wall_8.png" }
-  outputs.sprites << { x: 76 * 16,  y: 28 * 16, w: 48, h: 48, path: "sprites/wall_1.png" }
-
-  choose_junction_sprite(x: 1, y: 2)
+  outputs.sprites << { x: 1 * 16,   y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 2, y: 16)}.png" }
+  outputs.sprites << { x: 1 * 16,   y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 2, y: 29)}.png" }
+  outputs.sprites << { x: 76 * 16,  y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 77, y: 16)}.png" }
+  outputs.sprites << { x: 76 * 16,  y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 77, y: 29)}.png" }
 end
 
 # draw inner walls in room, forming a simple maze with wide corridors
@@ -129,6 +126,32 @@ def draw_inner_wall_sprites
   draw_wall_segment_sprites(x: 33, y: 17, dir: get_direction)
   draw_wall_segment_sprites(x: 48, y: 17, dir: get_direction)
   draw_wall_segment_sprites(x: 63, y: 17, dir: get_direction)
+end
+
+def draw_wall_junctions_sprites
+  outputs.sprites << { x: 31 * 16, y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y: 42)}.png" }
+  outputs.sprites << { x: 46 * 16, y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y: 42)}.png" }
+  outputs.sprites << { x: 1 * 16 , y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x:  2, y: 16)}.png" }
+  outputs.sprites << { x: 1 * 16 , y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x:  2, y: 29)}.png" }
+
+  outputs.sprites << { x: 16 * 16, y:  2 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 17, y:  3)}.png" }
+  outputs.sprites << { x: 16 * 16, y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 17, y: 42)}.png" }
+  outputs.sprites << { x: 61 * 16, y:  2 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 62, y:  3)}.png" }
+  outputs.sprites << { x: 61 * 16, y: 41 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 62, y: 42)}.png" }
+
+  outputs.sprites << { x: 31 * 16, y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y: 16)}.png" }
+  outputs.sprites << { x: 46 * 16, y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y: 16)}.png" }
+  outputs.sprites << { x: 61 * 16, y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 62, y: 16)}.png" }
+  outputs.sprites << { x: 16 * 16, y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 17, y: 16)}.png" }
+  outputs.sprites << { x: 31 * 16, y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y: 29)}.png" }
+  outputs.sprites << { x: 46 * 16, y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y: 29)}.png" }
+  outputs.sprites << { x: 61 * 16, y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 62, y: 29)}.png" }
+  outputs.sprites << { x: 16 * 16, y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 17, y: 29)}.png" }
+
+  outputs.sprites << { x: 76 * 16, y: 15 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 77, y: 16)}.png" }
+  outputs.sprites << { x: 76 * 16, y: 28 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 77, y: 29)}.png" }
+  outputs.sprites << { x: 31 * 16, y:  2 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 32, y:  3)}.png" }
+  outputs.sprites << { x: 46 * 16, y:  2 * 16, w: 48, h: 48, path: "sprites/wall_#{choose_junction_sprite(x: 47, y:  3)}.png" }
 end
 
 # function to draw wall segments, pass in the x, y coordinates, and the direction to draw the segment
@@ -184,28 +207,24 @@ def draw_wall_segment_sprites(x:, y:, dir:)
 end
 
 def choose_junction_sprite(x:, y:)
-  # x += 1
-  # y += 1
-  top = (y > 0 && state.room_grid[y-1][x] == 1) ? 1 : 0
-  right = (x < state.room_grid[0].length - 1 && state.room_grid[y][x+1] == 1) ? 1 : 0
-  bottom = (y < state.room_grid.length - 1 && state.room_grid[y+1][x] == 1) ? 1 : 0
-  left = (x > 0 && state.room_grid[y][x-1] == 1) ? 1 : 0
+  # we want to check the game grid coordinates at x: 3, y: 4, this is at x: 2, y: 3 in 2d array, since 0, 0 is bottom left corner
+  n = (state.room_grid[y + 1][x] == 1) ? 1 : 0
+  s = (state.room_grid[y - 1][x] == 1) ? 1 : 0
+  e = (state.room_grid[y][x + 1] == 1) ? 1 : 0
+  w = (state.room_grid[y][x - 1] == 1) ? 1 : 0
 
-=begin
-  puts60 "room_grid #{x}, #{y}: #{state.room_grid[y][x]}"
-  puts60 "top #{top} right #{right} bottom #{bottom} left #{left}"
+  # puts60 "room_grid #{x}, #{y}: #{state.room_grid[y][x]}"
+  # puts60 "n #{n} s #{s} e #{e} w #{w}"
+  # puts60 "#{s}#{e}#{w}#{n}".to_i(2)
 
-  puts60 "#{top}#{right}#{bottom}#{left}".to_i(2)
-
-  puts60 "#{state.room_grid[0][6]} #{state.room_grid[1][6]} #{state.room_grid[2][6]} #{state.room_grid[3][6]} #{state.room_grid[4][6]} #{state.room_grid[5][6]}"
-  puts60 "#{state.room_grid[0][5]} #{state.room_grid[1][5]} #{state.room_grid[2][5]} #{state.room_grid[3][5]} #{state.room_grid[4][5]} #{state.room_grid[5][5]}"
-  puts60 "#{state.room_grid[0][4]} #{state.room_grid[1][4]} #{state.room_grid[2][4]} #{state.room_grid[3][4]} #{state.room_grid[4][4]} #{state.room_grid[5][4]}"
-  puts60 "#{state.room_grid[0][3]} #{state.room_grid[1][3]} #{state.room_grid[2][3]} #{state.room_grid[3][3]} #{state.room_grid[4][3]} #{state.room_grid[5][3]}"
-  puts60 "#{state.room_grid[0][2]} #{state.room_grid[1][2]} #{state.room_grid[2][2]} #{state.room_grid[3][2]} #{state.room_grid[4][2]} #{state.room_grid[5][2]}"
-  puts60 "#{state.room_grid[0][1]} #{state.room_grid[1][1]} #{state.room_grid[2][1]} #{state.room_grid[3][1]} #{state.room_grid[4][1]} #{state.room_grid[5][1]}"
-  puts60 "#{state.room_grid[0][0]} #{state.room_grid[1][0]} #{state.room_grid[2][0]} #{state.room_grid[3][0]} #{state.room_grid[4][0]} #{state.room_grid[5][0]}"
-=end
-
+  # puts60 "#{state.room_grid[6][0]} #{state.room_grid[6][1]} #{state.room_grid[6][2]} #{state.room_grid[6][3]} #{state.room_grid[6][4]} #{state.room_grid[6][5]}"
+  # puts60 "#{state.room_grid[5][0]} #{state.room_grid[5][1]} #{state.room_grid[5][2]} #{state.room_grid[5][3]} #{state.room_grid[5][4]} #{state.room_grid[5][5]}"
+  # puts60 "#{state.room_grid[4][0]} #{state.room_grid[4][1]} #{state.room_grid[4][2]} #{state.room_grid[4][3]} #{state.room_grid[4][4]} #{state.room_grid[4][5]}"
+  # puts60 "#{state.room_grid[3][0]} #{state.room_grid[3][1]} #{state.room_grid[3][2]} #{state.room_grid[3][3]} #{state.room_grid[3][4]} #{state.room_grid[3][5]}"
+  # puts60 "#{state.room_grid[2][0]} #{state.room_grid[2][1]} #{state.room_grid[2][2]} #{state.room_grid[2][3]} #{state.room_grid[2][4]} #{state.room_grid[2][5]}"
+  # puts60 "#{state.room_grid[1][0]} #{state.room_grid[1][1]} #{state.room_grid[1][2]} #{state.room_grid[1][3]} #{state.room_grid[1][4]} #{state.room_grid[1][5]}"
+  # puts60 "#{state.room_grid[0][0]} #{state.room_grid[0][1]} #{state.room_grid[0][2]} #{state.room_grid[0][3]} #{state.room_grid[0][4]} #{state.room_grid[0][5]}"
+  "#{s}#{e}#{w}#{n}".to_i(2) # return autotile bits in decimal form
 end
 
 # this is a version of the generation system used in the arcade game berzerk - it follows the same patterns as the arcade game following a reset.
@@ -240,7 +259,7 @@ def draw_wall_debug
     outputs.solids << { x: x, y: y, w: 48, h: 48, r: 100, g: 100, b: 200 }
     x += state.segment_width - 16
     if x > 1280
-      x = 16 # 16 * 16
+      x = 16
       y += 13 * 16
     end
     count += 1
@@ -305,8 +324,8 @@ def draw_wall_debug
 end
 
 def set_defaults
-  state.rows = 720 / 16  # 45
-  state.cols = 1280 / 16 # 80
+  state.rows = 45 # 720 / 16
+  state.cols = 80 # 1280 / 16
   state.room_grid = nil
   state.segment_height = 16 * 12 + 2 * 16
   state.segment_width = 16 * 14 + 2 * 16
